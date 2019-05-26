33e0f24a6a2df47b2200a8839962d3d826ac7589
devdna <choi.yongwoong@gmail.com>
2019-05-26 18:34:15 +0900

    init

---
+++ src/test/java/me/devdna/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package me.devdna;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HelloWorld.
+ */
+public class HelloWorldTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
